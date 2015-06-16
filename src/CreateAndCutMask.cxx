e71c59fcf3c6691807dddcbe588c676041c1da90
tschuls <juliast@ethz.ch>
2015-06-16 15:38:45 +0200

    first approach with inside/outside check - not working

--- src/CreateAndCutMask.cxx
+++ src/CreateAndCutMask.cxx
@@ -20,12 +20,97 @@
 #include "itkImageSeriesReader.h"
 #include "itkImageSeriesWriter.h"
 
-//Filter for cutting
-#include "itkMaskNegatedImageFilter.h"
-
-
 #include <vector>
 #include "itksys/SystemTools.hxx"
+
+#include "itkPointSet.h"
+
+#include "itkImageRegionIterator.h"
+
+#include "itkVector.h"
+#include "itkMatrix.h"
+
+using namespace std;
+
+typedef signed short    PixelType;
+const unsigned int      Dimension = 3;
+typedef itk::Image< PixelType, Dimension >      ImageType;
+typedef itk::ImageSeriesReader< ImageType >     ReaderType;
+typedef ImageType::Pointer ImagePointerType;
+
+typedef itk::PointSet< double, 3 >   PointSetType;
+typedef PointSetType::PointType DoublePointType;
+typedef PointSetType::PointsContainer PointsContainerType;
+typedef PointsContainerType::Pointer PointsContainerPointer;
+
+typedef itk::Vector<double, 3> VectorType;
+typedef itk::Point<double,3> PointType;
+
+
+PointsContainerPointer readLandmarksWithOrigin(string filename, ImagePointerType image) {
+    if (filename==""){
+        return NULL;
+    }
+
+    std::vector<double> extent(Dimension);
+    ImageType::SpacingType spacing = image->GetSpacing();
+    ImageType::SizeType size = image->GetLargestPossibleRegion().GetSize();
+    for (int d=0;d<Dimension;++d) {
+        extent[d] = spacing[d] * size[d];
+    }
+
+
+    PointSetType::Pointer  pointSet = PointSetType::New();
+    PointsContainerPointer points=pointSet->GetPoints();
+
+    ifstream ifs(filename.c_str());
+
+    //read origin
+    DoublePointType origin;
+    for (int d=0;d<Dimension;++d) {
+        ifs>>origin[d];
+    }
+
+    int i=0;
+    while ( ! ifs.eof() ) {
+        DoublePointType point;
+        bool fullPoint=true;
+        for (int d=0;d<Dimension;++d){
+            ifs>>point[d];
+            if (ifs.eof()){
+                fullPoint=false;
+                break;
+            }
+
+        }
+        if (fullPoint){
+            //move point by: (Landmark-Origin)-Extent/2
+            point = point - origin;
+            for (int d=0;d<Dimension;++d) {
+                point[d] = point[d] - extent[d]/2;
+            }
+            points->InsertElement(i, point);
+            ++i;
+        }
+    } 
+    return points;
+}
+
+bool isInside(PointType point, double width, double height, double depth) {
+    //check if x smaller than width
+    //check if y smaller than height
+    //check if z smaller than depth
+    if (abs(point[0]) < width) {
+        if (abs(point[1]) < height) {
+            if (abs(point[2]) < depth) {
+                return true;
+            }
+        }
+    }
+    return false;
+
+}
+
 int main( int argc, char* argv[] )
 {
     if( argc < 4 )
@@ -34,27 +119,27 @@ int main( int argc, char* argv[] )
             " DicomInputDirectory  OutputDicomDirectory landmarkFile" << std::endl;
         return EXIT_FAILURE;
     }
-    typedef signed short    PixelType;
-    const unsigned int      Dimension = 3;
-    typedef itk::Image< PixelType, Dimension >      ImageType;
-    typedef itk::ImageSeriesReader< ImageType >     ReaderType;
-
+    
+    
+    //****************************************************************
+    //****** read input image ****************************************
+    //****************************************************************
     typedef itk::GDCMImageIO                        ImageIOType;
     typedef itk::GDCMSeriesFileNames                NamesGeneratorType;
+
     ImageIOType::Pointer gdcmIO = ImageIOType::New();
     NamesGeneratorType::Pointer namesGenerator = NamesGeneratorType::New();
 
     namesGenerator->SetInputDirectory( argv[1] );
-    const ReaderType::FileNamesContainer & filenames =
-        namesGenerator->GetInputFileNames();
+    const ReaderType::FileNamesContainer & filenames = namesGenerator->GetInputFileNames();
 
     const unsigned int numberOfFileNames =  filenames.size();
-    std::cout << numberOfFileNames << std::endl;
-    for(unsigned int fni = 0; fni < numberOfFileNames; ++fni)
-    {
-        std::cout << "filename # " << fni << " = ";
-        std::cout << filenames[fni] << std::endl;
-    }
+    //std::cout << numberOfFileNames << std::endl;
+    //for(unsigned int fni = 0; fni < numberOfFileNames; ++fni)
+    //{
+    //    std::cout << "filename # " << fni << " = ";
+    //    std::cout << filenames[fni] << std::endl;
+    //}
     ReaderType::Pointer reader = ReaderType::New();
     reader->SetImageIO( gdcmIO );
     reader->SetFileNames( filenames );
@@ -72,14 +157,82 @@ int main( int argc, char* argv[] )
 
     ImageType::Pointer inputImage = reader->GetOutput();
 
-    //get landmarks
-    //TODO
 
-    //remove part of input based on landmarks
-    //use parameters for width etc
-    //TODO
 
-    //write to outputDirectory
+    //****************************************************************
+    //****** read landmarks file *************************************
+    //****************************************************************
+    PointsContainerPointer landmarks = readLandmarksWithOrigin(argv[3], inputImage);
+
+    PointType eye1 = landmarks->GetElement(1);
+    PointType eye2 = landmarks->GetElement(2);
+    PointType mandible = landmarks->GetElement(3);
+    PointType nose = landmarks->GetElement(0);
+
+    //****************************************************************
+    //****** calculate values for inside/outside computation *********
+    //****************************************************************    
+    VectorType v1;
+    v1 = eye2 - eye1;
+
+    VectorType v2;
+    v2 = mandible - eye1;
+
+    VectorType v3 = itk::CrossProduct(v2,v1);
+
+    double width = v1.GetNorm();
+    double heigth = v2.GetNorm();
+    double depth = (nose - eye1).GetNorm();
+    
+    cout << "box has sizing: " << endl;
+    cout << "  width: " << width << endl;
+    cout << "  height: " << heigth << endl;
+    cout << "  depth: " << depth << endl;
+
+    
+    v1.Normalize();
+    v2.Normalize();
+    v3.Normalize();
+
+    //create matrix
+    typedef itk::Matrix<double, 3, 3> MatrixType;
+    MatrixType M;
+    M(0,0) = v1[0];
+    M(1,0) = v1[1];
+    M(2,0) = v1[2];
+    M(0,1) = v2[0];
+    M(1,1) = v2[1];
+    M(2,1) = v2[2];
+    M(0,2) = v3[0];
+    M(1,2) = v3[1];
+    M(2,2) = v3[2];
+
+    MatrixType inverseM = M.GetInverse();
+
+    //****************************************************************
+    //****** iterate over image **************************************
+    //****************************************************************      
+    typedef itk::ImageRegionIterator<ImageType> ItType;
+    ItType it( inputImage, inputImage->GetLargestPossibleRegion() );
+    it.GoToBegin();
+
+    while (!it.IsAtEnd()) {
+        PointType queryPoint;
+        inputImage->TransformIndexToPhysicalPoint(it.GetIndex(),queryPoint);
+
+        PointType newPoint = queryPoint - eye1;
+        PointType resultPoint = inverseM * newPoint;
+
+        if (!isInside(resultPoint, width,heigth,depth)) {
+            inputImage->SetPixel(it.GetIndex(), -1000);
+        }
+        ++it;
+    }
+
+
+    //****************************************************************
+    //****** write masked image out **********************************
+    //**************************************************************** 
     const char * outputDirectory = argv[2];
     itksys::SystemTools::MakeDirectory( outputDirectory );
     typedef signed short    OutputPixelType;
@@ -89,8 +242,7 @@ int main( int argc, char* argv[] )
     SeriesWriterType::Pointer seriesWriter = SeriesWriterType::New();
 
     //write identical headers also with original UID's -> besides the changed pixel values the files look the same!
-    //TODO
-    //seriesWriter->SetInput( maskFilter->GetOutput() );
+    seriesWriter->SetInput( inputImage );
 
     gdcmIO->SetKeepOriginalUID(true);
     seriesWriter->SetImageIO( gdcmIO );
@@ -107,6 +259,9 @@ int main( int argc, char* argv[] )
         std::cerr << excp << std::endl;
         return EXIT_FAILURE;
     }
-
     return EXIT_SUCCESS;
+
+
+
 }
+
